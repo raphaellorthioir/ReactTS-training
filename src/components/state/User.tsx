46c0fb853360a7592404553111ea7c6df5844559
raphael.lorthioir <raphael.lorthioir@gmail.com>
2023-06-26 16:10:51 +0200

    useState type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,22 +6,23 @@ type AuthUser = {
   email: string;
 };
 const User = () => {
-  const [user, setUser] = useState<null | AuthUser>(null);
+  //const [user, setUser] = useState<null | AuthUser>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser); // if we're sure that user would be initialized a soon as we logged in , we can initialize state with type Assertion 
   const handleLogin = () => {
     setUser({
       name: 'raf',
       email: 'raf@gmail.com',
     });
   };
-  const handleLogout = () => {
+  /* const handleLogout = () => {
     setUser(null);
-  };
+  };*/
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email adress is {user?.email}</div>
+      {/*  <button onClick={handleLogout}>Logout</button>*/}
+      <div>User name is {user.name}</div>
+      <div>User email adress is {user.email}</div>
     </div>
   );
 };
